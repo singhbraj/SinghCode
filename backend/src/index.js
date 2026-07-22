0c9efac17e509176bbed8c8c5034718820e37248
singhbraj <braj333singh@gmail.com>
2026-07-23 00:14:12 +0530

    Added websocket connetion

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,16 +2,29 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes/index.js';
 import { PORT } from './config/serverConfig.js';
-
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
 
 const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+});
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
 app.use('/api', router);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,12 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import { CreateProject } from './pages/CreateProject'
 import { Router } from './Routes'
+import {io} from 'socket.io-client'
 
 function App() {
+
+  const socket = io('http://localhost:3000')
+
   return (
 
    <Router />
